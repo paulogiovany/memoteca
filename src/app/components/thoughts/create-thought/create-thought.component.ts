6ac5b0e57343a7044e2c22697acfa510e6743e1f
Paulo Oliveira <paulo.giovany@prax.net.br>
2025-02-25 16:41:14 -0300

    feat/note creation form implemented

---
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thought',
+  templateUrl: './create-thought.component.html',
+  styleUrls: ['./create-thought.component.css']
+})
+export class CreateThoughtComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo:'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  createThought(): void {
+    alert('Creating thought')
+  }
+
+  cancelCreation(): void {
+
+  }
+}
